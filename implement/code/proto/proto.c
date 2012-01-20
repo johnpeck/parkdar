5cb79912f4f62cd1a2587e3fbbb90e1c57eb3a41
John Peck <john@hippo.(none)>
2012-01-19 18:28:05 -0800

    Mocked up the averaging function.

--- implement/code/proto/proto.c
+++ implement/code/proto/proto.c
@@ -1,16 +1,51 @@
 #include <stdio.h>
 
-int looper() {
+
+
+int looper(int *array) {
     for (int count = 0;count < 5; count++) {
+        printf("Array value is %d\n",array[count]);
         puts("My junk");
     };
     return 0;
 }
 
+/* getavg()
+ * Averages n numbers from an array, where n is the closest power of 2
+ * to avgnum */
+int getavg(int *array,int avgnum) {
+    int numshift = 0;
+    int sum = 0;
+    int average = 0;
+    int powcount = 0;
+    int avgshift = avgnum >> 1;
+    if (avgshift == 0)
+        average = array[0];
+    else {
+        while (avgshift != 0) {
+            powcount++;
+            avgshift = avgshift >> 1;
+        };
+        printf("Closest power of 2 is %d\n",powcount);
+        for (int count = 0; count < (1 << powcount); count++) {
+            sum += array[count];
+        };
+        average = sum >> powcount;
+    };
+    
+    return average;
 
-main()
+}
+
+
+
+
+int main()
 {
+    int myarray[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,\
+                       20};
     puts("Hello World!");
-    looper();
+    looper(myarray);
+    printf("Average value is %d\n",getavg(myarray,2));
     return 0;
 }
