08c31964b60227069c8db4b035ca8caee4e1896d
John Peck <john@hippo.(none)>
2012-03-15 16:54:15 -0700

    Worked some more on received character isr prototype.

--- implement/code/proto/proto.c
+++ implement/code/proto/proto.c
@@ -2,32 +2,37 @@
 #include <string.h>
 
 #define NUMCOMMANDS 5
-/* Define the size of the received character buffer */
+// Define the size of the received character buffer
 #define RECEIVE_BUFFER_SIZE 20
+// Define the size of the parse buffer
+#define PARSE_BUFFER_SIZE 20
+
 
 /* This will be like my received character buffer */
 char usart_receive_buffer[RECEIVE_BUFFER_SIZE]; 
 
-/* Receive character buffer state structure. The idea is that I'll create
+/* Received command state structure. The idea is that I'll create
  * a structure to keep track of the state in every flow diagram I make.
  * I have a flow diagram for received characters, so I created this
  * structure. */
 typedef struct { 
     char rbuffer[RECEIVE_BUFFER_SIZE]; // Received character buffer
-    int charcount; // Number of chars sent to receive buffer
+    char pbuffer[PARSE_BUFFER_SIZE]; // Parse buffer
+    int rbuffer_count; // Number of chars sent to receive buffer
     int pbuffer_lock; // Parse buffer lock.  1 = locked
-} rbuffer_state_t;
+} recv_cmd_state_t;
 // Define a pointer to the state
-rbuffer_state_t  rbuffer_state, *rbuffer_state_ptr = &rbuffer_state;
+recv_cmd_state_t  recv_cmd_state, *recv_cmd_state_ptr = &recv_cmd_state;
 
 
-/* Making this function explicitly take a pointer to the receive buffer
+/* Making this function explicitly take a pointer to the received command
  * state structure makes it clear that it modifies this structure.  This
  * function will ultimately also have to set up the USART hardware. */
-void usart_init( rbuffer_state_t *rbuffer_state_ptr ) {
-    memset((rbuffer_state_ptr -> rbuffer),0,RECEIVE_BUFFER_SIZE);
-    rbuffer_state_ptr -> charcount = 0;
-    rbuffer_state_ptr -> pbuffer_lock = 0; // Parse buffer unlocked
+void usart_init( recv_cmd_state_t *recv_cmd_state_ptr ) {
+    memset((recv_cmd_state_ptr -> rbuffer),0,RECEIVE_BUFFER_SIZE);
+    memset((recv_cmd_state_ptr -> pbuffer),0,PARSE_BUFFER_SIZE);
+    recv_cmd_state_ptr -> rbuffer_count = 0;
+    recv_cmd_state_ptr -> pbuffer_lock = 0; // Parse buffer unlocked
     return;
 }
 
@@ -161,11 +166,14 @@ char * cmdIdent(char * recString) {
 
 /* receive_isr_proto(char)
  * This mocks up the receive character interrupt of the AVR */
-void receive_isr_proto( rbuffer_state_t *rbuffer_state_ptr,
+void receive_isr_proto( recv_cmd_state_t *recv_cmd_state_ptr,
                          char *inchar) {
     if (!strcmp(inchar,"\r")) {
-        printf("It's a terminator");
-    };
+        printf("Found a terminator\r\n");
+    }
+    else {
+        printf("  <-- Not a terminator\r\n");
+    }
     return;
 }
 
@@ -192,8 +200,14 @@ int main()
         cmdIdent("range? 5"); // Send the command I want to check
     };
     if (do_isr_test != 0) {
-        usart_init( rbuffer_state_ptr );
-        //receive_isr_proto(rbuffer_state_ptr, "r");
+        usart_init( recv_cmd_state_ptr );
+        char teststr[] = "test", *teststr_ptr = teststr; 
+        for ( int index = 0; index < strlen(teststr); index++) {
+            putchar(teststr[index]); // Show which character is being sent
+            receive_isr_proto(recv_cmd_state_ptr,teststr_ptr);
+            teststr_ptr += 1;
+        };
+        printf("\r\n");
     };
     return 0;
 }
