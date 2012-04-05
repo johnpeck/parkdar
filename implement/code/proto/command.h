6a9aa72889a2b190988cb09708099209531dfa7a
John Peck <john@hippo.(none)>
2012-04-05 15:14:53 -0700

    Starting to separate out command interface from main.

---
+++ implement/code/proto/command.h
@@ -0,0 +1,42 @@
+// Define the size of the received character buffer
+#define RECEIVE_BUFFER_SIZE 20
+// Define the size of the parse buffer
+#define PARSE_BUFFER_SIZE 20
+
+/* Received command state structure. The idea is that I'll create
+ * a structure to keep track of the state in every flow diagram I make.
+ * I have a flow diagram for received characters, so I created this
+ * structure. */
+typedef struct { 
+    char rbuffer[RECEIVE_BUFFER_SIZE]; // Received character buffer
+    // rbuffer_write_ptr will always point to the next write location
+    char *rbuffer_write_ptr;
+    // Parse buffer.  pbuffer will point to the beginning of the parse
+    // buffer, which will also be the beginning of the command to be
+    // processed.
+    char pbuffer[PARSE_BUFFER_SIZE];
+    char *pbuffer_arg_ptr; // Points to the beginning of the argument
+    int rbuffer_count; // Number of chars sent to receive buffer
+    int pbuffer_lock; // Parse buffer lock.  1 = locked
+} recv_cmd_state_t;
+
+
+/* Define fpointer_t to have the type "pointer to function," a return
+ * value of void, and a parameter type of void. */
+typedef void (*fpointer_t)(void);
+/* Each command_struct will describe one command */
+struct command_struct {
+    char *name; // The name of the command
+    char *arg_type; // A string representing the type of argument expected
+    int arg_max_chars; // The maximum number of characters in the argument
+    fpointer_t execute; // The function to execute
+    char *help;
+};
+
+
+/* check_argsize( pointer to received command state,
+ *                pointer to list of commands )
+ * Returns 0 if the argument size is less than or equal to the number
+ * of characters specified in the command list.  Returns -1 otherwise. */
+int check_argsize(recv_cmd_state_t *recv_cmd_state_ptr ,
+                  struct command_struct *command_array);
