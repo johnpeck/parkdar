1dfbd744a1d3f8f0623d56963773dc411d1b0f1e
John Peck <john@hippo.(none)>
2012-04-10 11:06:41 -0700

    Added calibration structure to ranger.

--- implement/code/proto/pd_ranger.h
+++ implement/code/proto/pd_ranger.h
@@ -2,6 +2,11 @@
  * 
  * Functions for dealing with range finding */
 
+/* Calibration structure */
+struct cal_struct {
+    int slope; // Measurements are multiplied by this factor before offset
+    int offset; // Measurements are offset by this factor after slope is applied
+};
 
 
 /* This is the junk function.
